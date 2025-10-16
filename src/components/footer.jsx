96078ef8821a1adda8e09502c9cd236634909b75
priti-sinare <pritisinare99@gmail.com>
2025-10-16 19:37:46 +0530

    Added Sorting Visualizer

---
+++ src/components/footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Footer Component
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                Created by
+                <a href="https://www.linkedin.com/in/priti-sinare" target="_blank" rel="noopener noreferrer" className="link">
+                    <span> </span>
+                    Priti Sinare
+                </a>
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
